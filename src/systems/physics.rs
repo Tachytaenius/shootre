ce0c246b6864b6031b126e6341c6e15a88608e37
Tachytaenius <henryfthomson@gmail.com>
2023-05-27 11:11:07 +0100

    Add unoptimised proper collision detection

--- src/systems/physics.rs
+++ src/systems/physics.rs
@@ -7,7 +7,8 @@ use bevy_ecs_tilemap::prelude::*;
 pub fn collision(
     mut collider_query: Query<(&Collider, &mut Position, &mut Velocity, Option<&Mass>, Option<&Restitution>, Option<&Children>, Option<(&mut Hits, &HitForceThreshold)>)>,
     child_mass_query: Query<&Mass>,
-    wall_tilemap_query: Query<(&TilemapSize, &TileStorage), With<WallTilemap>>
+    wall_tilemap_query: Query<(&TilemapSize, &TileStorage), With<WallTilemap>>,
+    time: Res<Time>
 ) {
     // Entity-level collisions
     let (tilemap_size, tile_storage) = wall_tilemap_query.get_single().unwrap();
@@ -15,12 +16,36 @@ pub fn collision(
         entity_collider,
         mut entity_position,
         mut entity_velocity,
-        entity_mass_option,
+        _, // entity_mass_option,
         entity_restitution_option,
-        entity_children_option,
-        mut entity_hit_related_option
+        _, // entity_children_option,
+        _ // mut entity_hit_related_option
     ) in collider_query.iter_mut() {
-        // Non-solid colliders still can't pass through walls
+        // Non-solid colliders still can't pass through walls, so no guard clause here
+        
+        // Get mass and restitution
+        // Mass is unused with hits being scrapped to a "TODO"
+        // let mut entity_mass;
+        // if let Some(entity_mass_component) = entity_mass_option {
+        //     entity_mass = entity_mass_component.value;
+        //     if let Some(children) = entity_children_option {
+        //         for child_entity in children.iter() {
+        //             if let Ok(child_mass) = child_mass_query.get(*child_entity) {
+        //                 entity_mass += child_mass.value;
+        //             }
+        //         }
+        //     }
+        // } else {
+        //     entity_mass = 0.0;
+        // }
+        let entity_restitution = if let Some(entity_restitution_component) = entity_restitution_option {
+            entity_restitution_component.value
+        } else {
+            DEFAULT_RESTITUTION
+        };
+
+        // Do x axis
+        entity_position.value.x += entity_velocity.value.x * time.delta_seconds(); // Apply x velocity
         for x in 0..tilemap_size.x {
             for y in 0..tilemap_size.y {
                 // Guard clauses
@@ -36,26 +61,6 @@ pub fn collision(
                 ) {
                     continue;
                 }
-
-                // Get mass and restitution
-                let mut entity_mass;
-                if let Some(entity_mass_component) = entity_mass_option {
-                    entity_mass = entity_mass_component.value;
-                    if let Some(children) = entity_children_option {
-                        for child_entity in children.iter() {
-                            if let Ok(child_mass) = child_mass_query.get(*child_entity) {
-                                entity_mass += child_mass.value;
-                            }
-                        }
-                    }
-                } else {
-                    entity_mass = 0.0;
-                }
-                let entity_restitution = if let Some(entity_restitution_component) = entity_restitution_option {
-                    entity_restitution_component.value
-                } else {
-                    DEFAULT_RESTITUTION
-                };
 
                 let ( // Tilemap velocity/position changes are always  zero
                     (entity_velocity_change, _),
@@ -75,22 +80,65 @@ pub fn collision(
                     DEFAULT_RESTITUTION // TODO: Tilemap
                 );
 
-                entity_velocity.value += entity_velocity_change;
-                if let Some((ref mut hits, hit_force_threshold)) = entity_hit_related_option {
-                    let force = entity_velocity_change * entity_mass;
-                    if force.length() >= hit_force_threshold.value {
-                        hits.value.push(Hit {
-                            entry_point: Vec2::ZERO, // TODO
-                            force: force,
-                            damage: 0.0, // TODO
-                            apply_force: false,
-                            blood_loss: 0.0
-                        });
-                    }
-                }
-                entity_position.value += entity_position_change;
+                entity_velocity.value.x += entity_velocity_change.x;
+                entity_position.value.x += entity_position_change.x;
             }
         }
+
+        // Do y axis
+        entity_position.value.y += entity_velocity.value.y * time.delta_seconds(); // Apply y velocity
+        for x in 0..tilemap_size.x {
+            for y in 0..tilemap_size.y {
+                // Guard clauses
+                if tile_storage.get(&TilePos {x: x, y: y}).is_none() {
+                    continue;
+                }
+                if !collision_detection::circle_aabb(
+                    entity_collider.radius,
+                    entity_position.value,
+                    TILE_SIZE,
+                    TILE_SIZE,
+                    Vec2::new(x as f32, y as f32) * TILE_SIZE - Vec2::splat(TILE_SIZE / 2.0) + TILEMAP_OFFSET
+                ) {
+                    continue;
+                }
+
+                let ( // Tilemap velocity/position changes are always  zero
+                    (entity_velocity_change, _),
+                    (entity_position_change, _)
+                ) = collision_resolution::circle_aabb(
+                    entity_collider.radius,
+                    entity_position.value,
+                    entity_velocity.value,
+                    0.0, // Against a mass of 1, meaning the tilemap has "infinite mass"
+                    entity_restitution,
+
+                    TILE_SIZE,
+                    TILE_SIZE,
+                    Vec2::new(x as f32, y as f32) * TILE_SIZE - Vec2::splat(TILE_SIZE / 2.0) + TILEMAP_OFFSET,
+                    Vec2::ZERO,
+                    1.0,
+                    DEFAULT_RESTITUTION // TODO: Tilemap
+                );
+
+                entity_velocity.value.y += entity_velocity_change.y;
+                entity_position.value.y += entity_position_change.y;
+            }
+        }
+
+        // Scrapping entity-level hits to a "TODO", if you're moving fast enough for that you're going to clip through the walls
+        // if let Some((ref mut hits, hit_force_threshold)) = entity_hit_related_option {
+        //     let force = entity_velocity_change * entity_mass;
+        //     if force.length() >= hit_force_threshold.value {
+        //         hits.value.push(Hit {
+        //             entry_point: Vec2::ZERO, // TODO
+        //             force: force,
+        //             damage: 0.0, // TODO
+        //             apply_force: false,
+        //             blood_loss: 0.0
+        //         });
+        //     }
+        // }
     }
 
     // Entity-entity collisions
@@ -203,7 +251,7 @@ pub fn collision(
 }
 
 pub fn apply_velocity(
-    mut query: Query<(&mut Position, &Velocity)>,
+    mut query: Query<(&mut Position, &Velocity), Without<Collider>>, // Without collider because velocity is done in collision for colliders
     time: Res<Time>
 ) {
     for (mut position, velocity) in query.iter_mut() {
